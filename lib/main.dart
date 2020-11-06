1b5f22caeee9c170935f3ff6b1b131a379384964
uraK-AJP <50735263+uraK-AJP@users.noreply.github.com>
2020-11-06 19:49:09 +0900

    Map表示
    Mapが表示されるように＆マーカー追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -16,8 +18,9 @@ class MapsDemo extends StatefulWidget {
 }
 
 class MapsDemoState extends State<MapsDemo> {
-
   GoogleMapController mapController;
+  Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> _markers = {};
 
   @override
   Widget build(BuildContext context) {
@@ -27,24 +30,31 @@ class MapsDemoState extends State<MapsDemo> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: GoogleMap(
-          onMapCreated: _onMapCreated,
+            markers:_markers,
+            onMapCreated: _onMapCreated,
             initialCameraPosition: CameraPosition(
-              target: LatLng(35.6580339,139.7016358),
+              target: LatLng(35.6580339, 139.7016358),
               zoom: 17.0,
-            )
-        ),
+            )),
       ),
     );
   }
 
   void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+    mapController = controller;
     setState(() {
-      mapController = controller;
-      var options = MarkerOptions(
-          position: LatLng(35.6580339,139.7016358),
-          infoWindowText: InfoWindowText("タイトル", "説明分等")
-      );
-      mapController.addMarker(options);
+      _markers.add(Marker(
+        markerId: MarkerId(''),
+        position: LatLng(35.6580339, 139.7016358),
+        // infoWindowText: InfoWindowText("タイトル", "説明分等")
+      ));
+      _markers.add(Marker(
+        markerId: MarkerId(''),
+        position: LatLng(50.6580339, 140.7016358),
+        // infoWindowText: InfoWindowText("タイトル", "説明分等")
+      ));
+      // mapController.add(options);
     });
   }
 }
